d2dbdc6d205ec010d15db326821aff96def73d8b
FukCup <1224894560@qq.com>
2019-12-13 17:15:55 +0800

    1

--- routers/api.go
+++ routers/api.go
@@ -1,7 +1,7 @@
 package routers
 
 import (
-	"QUZHIYOU/app/controllers/activity"
+	"QUZHIYOU/app/http/controllers/activity"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,6 +9,7 @@ func InitRouter() *gin.Engine {
 
 	// 初始化默认路由
 	router:=gin.Default()
+
 
 	//活动相关的API
 	router.GET("/wechat/activity/selectActivityList", activity.ActivityList)
