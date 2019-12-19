309f55cd9e8e7a4878ad7187e15a15f9b01a4926
FukCup <1224894560@qq.com>
2019-12-19 17:04:24 +0800

    1

--- app/http/middleware/jwt.go
+++ app/http/middleware/jwt.go
@@ -1,13 +1,11 @@
 package middleware
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
 func JWTAuth() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		token := c.Request.Header.Get("userId")
-		fmt.Println(token,"---------token----------")
+		 c.Request.Header.Get("userId")
 	}
 }
