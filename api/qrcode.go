679d5341c7ceb43ed4f70cf8244708dea022719d
FukCup <1224894560@qq.com>
2019-12-30 02:42:52 +0800

    fukcup
    gin 封装

--- api/qrcode.go
+++ api/qrcode.go
@@ -2,29 +2,26 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/medivhzhan/weapp/v2"
 	"io/ioutil"
 	"os"
-	"github.com/medivhzhan/weapp/v2"
+	"path/filepath"
+)
+
+var (
+	token string
+	dir string
 )
 
 
-var token string
-
-func init() {
-	res, err := weapp.GetAccessToken("wx27f763c834a6e9aa", "2ac10ca85c83589fde470f990d6ad02e")
-	if err != nil {
-
-		return
-	}
-	if err := res.GetResponseError(); err != nil {
-
-		return
-	}
-
-	token = res.AccessToken
-}
-
 func Getqrcode(c *gin.Context) {
+
+	//获取文件路径
+	dir, _ = filepath.Abs(filepath.Dir(os.Args[0]))
+
+	token, _ := weapp.GetAccessToken(os.Getenv("WXAPP_ID"), os.Getenv("WXSECRET"))
+
+
 	getter := weapp.UnlimitedQRCode{
 		Scene:     "id=1",
 		Page:      "homeSub/pages/homeDetail/homeDetail",
@@ -34,7 +31,8 @@ func Getqrcode(c *gin.Context) {
 		IsHyaline: false,
 	}
 
-	resp, res, err := getter.Get(token)
+	resp, res, err := getter.Get(token.AccessToken)
+
 	if err != nil {
 		return
 	}
@@ -45,10 +43,22 @@ func Getqrcode(c *gin.Context) {
 
 	content, err := ioutil.ReadAll(resp.Body)
 
-	f,_:=os.Create("code.png")
+	f,_:=os.Create(dir+"/static/"+"code.png")
 
 	defer f.Close()
 
 	f.Write(content)
+
+
+	//上传图片到服务器 -no
+
+
+	c.JSON(200,gin.H{
+		"code":0,
+		"msg":"生成成功",
+		"data":dir+"/static/"+"code.png",
+	})
+
+
 
 }
