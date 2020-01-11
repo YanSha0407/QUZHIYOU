3a5b3272c992ce6c72ed2eee305f7aea395d1d66
FukCup <1224894560@qq.com>
2020-01-11 11:44:12 +0800

    1
    1

--- serializer/home_community_ser.go
+++ serializer/home_community_ser.go
@@ -7,6 +7,7 @@ import (
 	"golang.org/x/text/encoding/simplifiedchinese"
 	"golang.org/x/text/transform"
 	"io/ioutil"
+	"sort"
 )
 
 //序列化1
@@ -14,7 +15,7 @@ type Community struct {
 	Id      uint   `json:"id"`
 	Name    string `json:"cityName"`
 	KeyWord string `json:"keyword"`
-	Letter  string `json:"-"`
+	Letter  string `json:"letter"`
 }
 
 //返回结果序列化2
@@ -75,7 +76,7 @@ func GBK2UTF8(src []byte) (string, error) {
 func BuildCommunitys(item []models.Communitys) (l1 Lists) {
 
 	//强转类型
-	//sort.Sort(Animals(item))
+	sort.Sort(Animals(item))
 
 
 	fmt.Println(item,"item-----------")
